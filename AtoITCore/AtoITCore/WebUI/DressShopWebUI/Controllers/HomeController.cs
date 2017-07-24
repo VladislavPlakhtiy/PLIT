9964dc80f299c706648fa01741afd9a10ec7b6b7
SergeyDerko <sergeyderko1986@yandex.ru>
2017-07-24 16:22:58 +0300

    add Slider in _Layout

--- AtoITCore/AtoITCore/WebUI/DressShopWebUI/Controllers/HomeController.cs
+++ AtoITCore/AtoITCore/WebUI/DressShopWebUI/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web.Mvc;
 using Domain.Concrete;
 using Domain.Entityes;
@@ -14,17 +15,19 @@ namespace DressShopWebUI.Controllers
     // контролер, для работы с основными страницами сайта, кроме админ - панели
     public class HomeController : Controller
     {
-        private readonly ShopContext _db = new ShopContext(); //переменная контекста, по хорошему нужно еще и закрывать соединение с базой, подумаю...
+        private static readonly ShopContext Db = new ShopContext(); //переменная контекста, по хорошему нужно еще и закрывать соединение с базой, подумаю...
+
+        //Формируем список фотографий для слайдера и передаем его в _Layout
+        public static readonly IQueryable<Photo> Photo = from s in Db.Photo where s.Priority == true select s;
+        public static readonly List<Photo> SliderPhoto = Photo.ToList(); 
 
 
         public ViewResult Index()// Стартовая страница 
         {
-            DbSet<Photo> product = _db.Photo;
+            DbSet<Photo> product = Db.Photo;
             if (!product.Any()) // проверка на наличие БД и записей.
                 DebugDb.AddToDb();// Если БД пустая - заполнит "тестовыми значениями"
-            // выборка фотографий в слайдер
-            IQueryable<Photo> photo = from s in _db.Photo where s.Priority == true select s;
-            return View(photo.ToList());
+            return View();
         }
 
 
@@ -32,10 +35,10 @@ namespace DressShopWebUI.Controllers
         {
             
             //выбираем товары по категории  - 1
-            var selling = from a in _db.Photo
-                          where a.Product.Category == 1
-                          orderby a.Product.DateCreate descending
-                          select a;
+            var selling = from s in Db.Photo
+                          where s.Product.Category ==1
+                          orderby s.Product.DateCreate descending
+                          select s;
             return View(selling.ToList());
         }
 
@@ -43,22 +46,21 @@ namespace DressShopWebUI.Controllers
         public ActionResult Gallery() // страница Галерея
         {
             // выбираем фотографии по приоритету, и по категории 2
-            var photo = from a in _db.Photo
-                        where a.Priority == true
-                        where a.Product.Category == 2
-                        orderby a.Product.DateCreate descending
-                        select a;
+            var photo = from s in Db.Photo
+                        where s.Product.Category == 2
+                        orderby s.Product.DateCreate descending
+                        select s;
             return View(photo.ToList());
         }
 
 
         public ActionResult Partners() // страница Партнеры - в разработке!!!!
         {
-            IOrderedQueryable<Photo> partners = from a in _db.Photo
-                           where a.Product.Category == 3
-                           orderby a.Product.DateCreate descending
-                           select a;
-           return View(partners.ToList());
+            var partners = from s in Db.Photo
+                           where s.Product.Category == 3
+                           orderby s.Product.DateCreate descending
+                           select s;
+            return View(partners.ToList());
         }
 
 
@@ -70,16 +72,16 @@ namespace DressShopWebUI.Controllers
                     //проверяем наличие рейтинга заполненого пользователем.
                     var rating = string.IsNullOrEmpty(reviewStars) ? 0 : int.Parse(reviewStars);
                     // добавляем запись в БД
-                    _db.Reviews.Add(new Reviews
+                    Db.Reviews.Add(new Reviews
                     {
                         ClientName = name,
                         ClientFeedback = review,
                         Rating = rating,
                         DateFeedback = Now
                     });
-                    _db.SaveChanges();
+                    Db.SaveChanges();
                 }
-            var reviews = from i in _db.Reviews
+            var reviews = from i in Db.Reviews
                     orderby i.DateFeedback descending 
                     select i;
             
@@ -87,7 +89,5 @@ namespace DressShopWebUI.Controllers
             return View(reviews.ToList());
         }
 
-
-        
     }
 }
