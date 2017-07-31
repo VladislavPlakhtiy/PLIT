05523f1f248fb83e35256b51bc4811eef77f30a4
SergeyDerko <sergeyderko1986@yandex.ru>
2017-07-31 23:40:26 +0300

    Added methods to the Admin controller and views (initial implementation)

--- AtoITCore/AtoITCore/WebUI/DressShopWebUI/Controllers/AdminController.cs
+++ AtoITCore/AtoITCore/WebUI/DressShopWebUI/Controllers/AdminController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using Domain.Concrete;
 
 namespace DressShopWebUI.Controllers
 {
@@ -12,31 +10,67 @@ namespace DressShopWebUI.Controllers
     [Authorize]
     public class AdminController : Controller
     {
-        /// <summary>
-        /// Стартовая страница Админ-панели
-        /// </summary>
-        /// <returns></returns>
+        private static readonly ShopContext Db = new ShopContext();
+       
+        // Стартовая страница Админ-панели
         public ActionResult MyPanel()
         {
-            return View();
+            var product = from s in Db.Photo
+                           where s.Priority == true
+                           orderby s.Product.DateCreate descending
+                           select s;
+            return View(product.ToList());
         }
 
-        /// <summary>
-        /// добавления товара
-        /// </summary>
-        /// <returns></returns>
+        
+        // Редактирование товара
+        public ViewResult EditProduct(int productId)
+        {
+            var product = Db.Product.FirstOrDefault(x => x.ProductId == productId);
+            return View(product);
+        }
+
+
+        //удаление товара
+        public ViewResult DeleteProduct(int productId)
+        {
+            var product = Db.Product.FirstOrDefault(x => x.ProductId == productId);
+            return View(product);
+        }
+
+
+        // добавления товара
         public ActionResult AddProduct()
         {
             return View();
         }
 
-        /// <summary>
-        /// Список отзывов
-        /// </summary>
-        /// <returns></returns>
+      
+        // Список отзывов
         public ActionResult EditingReviews()
         {
-            return View();
+            var reviews = from s in Db.Reviews
+                          orderby s.DateFeedback descending
+                          select s;
+            return View(reviews.ToList());
         }
+
+
+        // редактирование отзыва
+        public ViewResult EditReview(int reviewId)
+        {
+            var review = Db.Reviews.FirstOrDefault(x => x.ReviewId == reviewId);
+            return View(review);
+        }
+
+
+        //удаление отзыва
+        public ViewResult DeleteReviews(int reviewId)
+        {
+            var review = Db.Reviews.FirstOrDefault(x => x.ReviewId == reviewId);
+            return View(review);
+        }
+        
+
     }
 }
